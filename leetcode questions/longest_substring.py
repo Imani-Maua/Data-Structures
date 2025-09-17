761437705e5eed68d5cb887364581004e768c5d9
Maua Imani <imanimaua@gmail.com>
2025-09-17 10:46:09 +0200

    Sliding windows technique

---
+++ leetcode questions/longest_substring.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        mystring = set()
+
+
+myset = set()
+string = "abcabcbb"
+for s in string:
+    myset.add(s)
+
+
+print(myset)
---
+++ leetcode questions/sliding_maxsubarray.py
@@ -0,0 +1,12 @@
+def maxsum(arr, k):
+    n = len(arr)
+    current_sum = sum(arr[:k])
+    max_sum = sum(arr[:k])
+
+    for i in range(k, n-1):
+        current_sum =+ arr[i] - arr[i-k]
+        max_sum = max(max_sum, current_sum)
+    
+    return max_sum
+
+
