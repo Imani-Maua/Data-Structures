8d38f5fafbf097718956f64219aef78d2826c3d5
Maua Imani <imanimaua@gmail.com>
2025-09-17 11:54:32 +0200

    Sliding windows 2

--- leetcode questions/sliding_maxsubarray.py
+++ leetcode questions/sliding_maxsubarray.py
@@ -1,10 +1,10 @@
-def maxsum(arr, k):
+def maxSum(arr, k):
     n = len(arr)
     current_sum = sum(arr[:k])
     max_sum = sum(arr[:k])
 
-    for i in range(k, n-1):
-        current_sum =+ arr[i] - arr[i-k]
+    for i in range(k, n):
+        current_sum += arr[i] - arr[i-k]
         max_sum = max(max_sum, current_sum)
     
     return max_sum
---
+++ leetcode questions/smallest_window.py
@@ -0,0 +1,23 @@
+def smallestWindow(arr):
+    if len(arr) < 3:
+        return -1
+    min_len = float('inf')
+    left = 0
+    freq = {}
+    for right in range(len(arr)):
+        if arr[right] == '1' or arr[right] == '2' or arr[right] == '0':
+            freq[arr[right]] = freq.get(arr[right], 0) + 1
+            while len(freq) == 3:
+                min_len = min(min_len, (right+1) - left)
+                freq[arr[left]] -= 1
+                if freq[arr[left]] == 0:
+                    del freq[arr[left]]
+                left+=1
+    if min_len == float('inf'):
+        return -1
+    return min_len
+
+
+
+
+
