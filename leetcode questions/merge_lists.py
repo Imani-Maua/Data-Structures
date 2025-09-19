5fad7d48023ce9722e1a1008b20c1e4ba287dae7
Maua Imani <imanimaua@gmail.com>
2025-09-19 19:02:24 +0200

    merge lists

---
+++ leetcode questions/merge_lists.py
@@ -0,0 +1,34 @@
+def mergeTwoLists(ls1, ls2):
+    
+    p1 = ls1
+    p2 = ls2
+    if not p1:
+        return p2
+    if not p2:
+        return p1
+    head = None
+    curr = None
+    while p1 and p2:
+        if not head:
+            if p1.val <= p2.val:
+                head = ListNode(p1.val)
+                curr = head
+            else:
+                head = ListNode(p2.val)
+                curr = head
+        else:
+            if p1.val <= p2.val:
+                newNode = ListNode(p1.val)
+                curr.next  = newNode
+                curr =newNode
+                p1 = p1.next
+            else:
+                newNode = ListNode(p2.val)
+                curr.next = newNode
+                curr = newNode
+                p2 = p2.next
+    curr.next = p1 if p1 else p2
+    return head
+
+
+        
